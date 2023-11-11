8605c0969155de723cc7cf419427bebb50cda7fb
nitpicker55555 <1303385763@qq.com>
2023-11-11 23:17:16 +0100

    process result

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import os,re
 import openai
 from blinker import Signal
 from dotenv import load_dotenv
-from flask import render_template, Flask, request, Response, stream_with_context,redirect,url_for
+from flask import render_template, Flask, request, Response, stream_with_context,jsonify,url_for,session
 load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv('OPENAI_API_KEY')
-
+app.secret_key = 'your_very_secret_key_here'  # 设置一个安全的密钥
 streaming_state = {'value': True}
 streaming_stopped = Signal()
 
@@ -52,7 +52,7 @@ def landing():
         whole_response="".join(response_list)
         length_token=judge_token(whole_response)
         print(length_token)
-        show_str(length_token)
+        session['variable'] = length_token
 
 
     return Response(stream_response(), mimetype='text/html')
@@ -66,13 +66,14 @@ def judge_token(text):
         # 非中文文本，按空格分割计算token数量
         tokens = text.split()
         return len(tokens)
-def show_str(length_token):
-    print(length_token)
-    return render_template('chat.html', my_string=length_token)
-@app.route('/stream_finished')
-def stream_finished():
-    # 流式处理完成后重定向
-    return redirect(url_for('show_result'))
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    print("process")
+    data = request.json
+    length=judge_token(data['value'])
+    # 对数据进行处理，例如仅返回原样的值
+    return jsonify({'response': 'Received: ' + data['value']+" length:"+str(length)})
+
 @app.route('/stop', methods=['GET'])
 def stop_streaming():
     print("stopped")
